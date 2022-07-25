2fb8bee965dca26750222989d4185311977bcf8f
sho watanabe <showatanabe0226@gmail.com>
2022-07-25 23:43:40 +0900

    [add]試合登録ページを追加

--- resources/js/Pages/Game/Subscribe.tsx
+++ resources/js/Pages/Game/Subscribe.tsx
@@ -1,7 +1,28 @@
 import Authenticated from "@/Layouts/Authenticated";
 import React from "react";
+import { Inertia } from "@inertiajs/inertia";
+import { useForm } from "@inertiajs/inertia-react";
+import CalcDate from "./Components/CalcDate";
 
 const Subscribe = (props: any) => {
+    const { teams, series } = props;
+
+    const today = CalcDate();
+
+    const { data, setData } = useForm({
+        home_team_id: "1",
+        away_team_id: "1",
+        matched_at: today,
+        series_id: "1",
+        home_team_point: 100,
+        away_team_point: 100,
+    });
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        Inertia.post(route("games.store"), data);
+    };
+
     return (
         <Authenticated
             auth={props.auth}
@@ -11,7 +32,119 @@ const Subscribe = (props: any) => {
                 </h2>
             }
         >
-            test
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <div className="flex">
+                        <div>
+                            <div>HOME</div>
+                            <div>
+                                <select
+                                    onChange={(e) =>
+                                        setData("home_team_id", e.target.value)
+                                    }
+                                >
+                                    {teams.map(
+                                        (data: {
+                                            id: number;
+                                            name: string;
+                                        }) => {
+                                            return (
+                                                <option
+                                                    key={data.id}
+                                                    value={data.id}
+                                                >
+                                                    {data.name}
+                                                </option>
+                                            );
+                                        }
+                                    )}
+                                </select>
+                            </div>
+                            <div>
+                                <input
+                                    type="number"
+                                    value={data.home_team_point}
+                                    onChange={(e: any) =>
+                                        setData(
+                                            "home_team_point",
+                                            e.target.value
+                                        )
+                                    }
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <div>AWAY</div>
+                            <div>
+                                <select
+                                    onChange={(e) =>
+                                        setData("away_team_id", e.target.value)
+                                    }
+                                >
+                                    {teams
+                                        .reverse()
+                                        .map(
+                                            (data: {
+                                                id: number;
+                                                name: string;
+                                            }) => {
+                                                return (
+                                                    <option
+                                                        key={data.id}
+                                                        value={data.id}
+                                                    >
+                                                        {data.name}
+                                                    </option>
+                                                );
+                                            }
+                                        )}
+                                </select>
+                            </div>
+                            <div>
+                                <input
+                                    type="number"
+                                    value={data.home_team_point}
+                                    onChange={(e: any) =>
+                                        setData(
+                                            "home_team_point",
+                                            e.target.value
+                                        )
+                                    }
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+                        <label>MATCH DAY</label>
+                        <input
+                            type="date"
+                            value={data.matched_at}
+                            onChange={(e) =>
+                                setData("matched_at", e.target.value)
+                            }
+                        />
+                    </div>
+                    <div>
+                        <label>SERIES</label>
+                        <select>
+                            {series.map(
+                                (data: { id: number; name: string }) => {
+                                    return (
+                                        <option key={data.id} value={data.id}>
+                                            {data.name}
+                                        </option>
+                                    );
+                                }
+                            )}
+                        </select>
+                    </div>
+                    <div>
+                        <button>Send</button>
+                    </div>
+                </form>
+            </div>
         </Authenticated>
     );
 };
