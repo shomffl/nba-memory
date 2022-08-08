69f2034ab690f61be65d17b52f8d7a17c2092a23
sho watanabe <showatanabe0226@gmail.com>
2022-08-08 14:55:38 +0900

    [add]Lazy Loading防止用のコードを追加

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Database\Eloquent\Model;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Model::preventLazyLoading(! $this->app->isProduction());
+
         Gate::define("admin", function($user) {
             return ($user->role == 0);
         });
