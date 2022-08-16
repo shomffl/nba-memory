62d2d6793231b296c26afba9c0fcbc0e041f8a23
sho watanabe <showatanabe0226@gmail.com>
2022-08-17 02:02:45 +0900

    [update]カレンダーと感想・試合ボックスの幅を修正

--- resources/js/Pages/Game/Index.tsx
+++ resources/js/Pages/Game/Index.tsx
@@ -73,14 +73,14 @@ const Index = (props: any) => {
         <Authenticated auth={props.auth} header={null}>
             <Head title="PULL ORIGIN"></Head>
             <div className="flex px-10 py-5">
-                <div className="w-9/12">
+                <div className="w-8/12">
                     <Calendar
                         schedules={schedules}
                         handleEventClick={handleEventClick}
                         handleDateClick={handleDateClick}
                     />
                 </div>
-                <div className="flex flex-col w-3/12">
+                <div className="flex flex-col w-4/12">
                     <div className="flex justify-center items-center font-bold h-12 ml-5 mb-5 bg-gray-100 rounded shadow-xl">
                         <h1>{todayGames[0]?.matched_at || "no match"}</h1>
                     </div>
