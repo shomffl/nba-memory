e48b90325adccab3dfacb40ab54e73193cb708f1
sho watanabe <showatanabe0226@gmail.com>
2022-08-18 12:23:30 +0900

    [update]感想編集画面のレイアウトを修正

--- resources/js/Pages/Post/Edit.tsx
+++ resources/js/Pages/Post/Edit.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Head, useForm } from "@inertiajs/inertia-react";
+import { Head, useForm, Link } from "@inertiajs/inertia-react";
 import Authenticated from "@/Layouts/Authenticated";
 import { Inertia } from "@inertiajs/inertia";
 
@@ -21,40 +21,77 @@ const Edit = (props: any) => {
             <Head title="PULL ORIGIN" />
 
             <form onSubmit={handleEditPost}>
-                <div className="px-40 py-20">
-                    <h1 className="text-5xl p-2">
-                        <input
-                            className="border border-black "
-                            value={data.title}
-                            onChange={(e) => setData("title", e.target.value)}
-                        />
-                    </h1>
-                    <div>
-                        <h2 className="p-2 pr-5">{post.game.matched_at}</h2>
+                <div className="px-28 py-20">
+                    <div className="bg-gray-100 rounded py-20 px-20 shadow-xl">
+                        <div className="pb-10">
+                            <h2>Match</h2>
+                            <div className="flex">
+                                <h1 className="bg-white text-3xl rounded px-5 py-2 mr-2 shadow">
+                                    {post.game.matched_at}
+                                </h1>
 
-                        <div>
-                            <h2 className="p-2 pr-5">
-                                {post.game.home_team.name} vs{" "}
-                                {post.game.away_team.name}
-                            </h2>
-                            <h1 className="p-2 pr-5">
-                                {post.game.home_team_point} vs{" "}
-                                {post.game.away_team_point}
-                            </h1>
+                                <h1 className="flex items-center bg-white text-3xl rounded px-5 py-2 mr-2 shadow">
+                                    <img
+                                        className="w-8 max-h-8"
+                                        src={post.game.home_team.logo}
+                                    />
+                                    &nbsp;
+                                    {post.game.home_team.name}&nbsp;(
+                                    {post.game.home_team_point})&nbsp;vs&nbsp;
+                                    {post.game.away_team.name}&nbsp;(
+                                    {post.game.away_team_point})&nbsp;
+                                    <img
+                                        className="w-8 max-h-8"
+                                        src={post.game.away_team.logo}
+                                    />
+                                </h1>
+                            </div>
                         </div>
-                        <div>
+
+                        <div className="pb-10">
+                            <h2>Title</h2>
+
+                            <input
+                                className="w-full rounded border-gray-1000 focus:border-gray-1000 focus:ring-1 focus:ring-gray-1000 text-3xl"
+                                type="text"
+                                value={data.title}
+                                onChange={(e) =>
+                                    setData("title", e.target.value)
+                                }
+                            />
+                            <label className="text-red-1000">
+                                {props.errors.title}
+                            </label>
+                        </div>
+
+                        <div className="pb-10">
+                            <h2>Detail</h2>
                             <textarea
+                                className="w-full rounded border-gray-1000 focus:border-gray-1000 focus:ring-1 focus:ring-gray-1000"
+                                rows={10}
                                 value={data.detail}
                                 onChange={(e) =>
                                     setData("detail", e.target.value)
                                 }
                             ></textarea>
+                            <label className="text-red-1000">
+                                {props.errors.detail}
+                            </label>
                         </div>
-                    </div>
-                    <div>
-                        <button className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 px-3 rounded">
-                            save
-                        </button>
+                        <div className="flex justify-between">
+                            <Link
+                                href={route("posts.show", post.id)}
+                                className="bg-gray-1000 text-white hover:text-white hover:bg-red-900 shadow hover:shadow-2xl hover:scale-105 px-5 py-1 rounded duration-200"
+                            >
+                                Back
+                            </Link>
+                            <button
+                                className="bg-gray-1000 text-white hover:text-white hover:bg-blue-1000 shadow hover:shadow-2xl hover:scale-105 px-5 py-1 rounded duration-200"
+                                type="submit"
+                            >
+                                Save
+                            </button>
+                        </div>
                     </div>
                 </div>
             </form>
