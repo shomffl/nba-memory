d8ed4b3372716eef13c5295275d15a1c6e2701c6
sho watanabe <showatanabe0226@gmail.com>
2022-08-25 15:33:15 +0900

    [update]表示する感想をシーズンで絞り込めるように修正

--- resources/js/Pages/Post/Index.tsx
+++ resources/js/Pages/Post/Index.tsx
@@ -1,13 +1,34 @@
 import React, { useState } from "react";
 import Authenticated from "@/Layouts/Authenticated";
+import { Inertia } from "@inertiajs/inertia";
 
-const Index = (props: { auth: any; posts: any }) => {
-    const { posts } = props;
+const Index = (props: { auth: any; posts: Array<Post>; seasons: any }) => {
+    const { posts, seasons } = props;
+    const [season, setSeason] = useState(seasons[0].id);
+
     return (
         <Authenticated auth={props.auth} header={null}>
             <div className="px-28 py-20">
                 <div>
-                    {posts.map((post: Post) => (
+                    <select onChange={(e) => setSeason(e.target.value)}>
+                        {seasons.map((season: any) => (
+                            <option key={season.id} value={season.id}>
+                                {season.season}
+                            </option>
+                        ))}
+                    </select>
+
+                    <button
+                        className="bg-blue-1000 text-white px-3 py-1 rounded-lg"
+                        onClick={(e) =>
+                            Inertia.get(route("posts.index"), {
+                                season: season,
+                            })
+                        }
+                    >
+                        send
+                    </button>
+                    {posts.map((post) => (
                         <div
                             className="bg-gray-100 my-10 pb-5 px-10 rounded-md shadow-lg"
                             key={post.id}
