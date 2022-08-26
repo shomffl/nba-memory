816d5b62fd31a9731f50789c1d2cfdd5bcae4518
sho watanabe <showatanabe0226@gmail.com>
2022-08-26 19:15:38 +0900

    [update]Index.tsxに渡すシーズンデータを降順に並び替え

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -46,7 +46,7 @@ class PostController extends Controller
             }
 
         }
-        return Inertia::render("Post/Index",["posts" => $posts->get(), "seasons" => $seasons->get()]);
+        return Inertia::render("Post/Index",["posts" => $posts->get(), "seasons" => $seasons->orderBy("season", "DESC")->get()]);
     }
 
     /**
