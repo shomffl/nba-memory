ffecadd9af1db426e143376b7768e31956504609
sho watanabe <showatanabe0226@gmail.com>
2022-10-22 14:56:39 +0900

    [update]シーダーの内容を変更

--- database/seeders/PostsTableSeeder.php
+++ database/seeders/PostsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use App\Models\Post;
 
 class PostsTableSeeder extends Seeder
@@ -15,6 +16,78 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        Post::factory()->count(500)->create();
+        // Post::factory()->count(100)->create();
+        DB::table("posts")->insert([
+            "user_id" => 1,
+            "game_id" => 1570,
+            "title" => "test",
+            "detail" => "test",
+            "created_at" => date("Y-m-d H:i:s"),
+            "updated_at" => date("Y-m-d H:i:s")
+        ]);
+        DB::table("posts")->insert([
+            "user_id" => 1,
+            "game_id" => 1571,
+            "title" => "test",
+            "detail" => "test",
+            "created_at" => date("Y-m-d H:i:s"),
+            "updated_at" => date("Y-m-d H:i:s")
+        ]);
+        DB::table("posts")->insert([
+            "user_id" => 1,
+            "game_id" => 1572,
+            "title" => "test",
+            "detail" => "test",
+            "created_at" => date("Y-m-d H:i:s"),
+            "updated_at" => date("Y-m-d H:i:s")
+        ]);
+        DB::table("posts")->insert([
+            "user_id" => 1,
+            "game_id" => 1573,
+            "title" => "test",
+            "detail" => "test",
+            "created_at" => date("Y-m-d H:i:s"),
+            "updated_at" => date("Y-m-d H:i:s")
+        ]);
+        DB::table("posts")->insert([
+            "user_id" => 1,
+            "game_id" => 1574,
+            "title" => "test",
+            "detail" => "test",
+            "created_at" => date("Y-m-d H:i:s"),
+            "updated_at" => date("Y-m-d H:i:s")
+        ]);
+        DB::table("posts")->insert([
+            "user_id" => 1,
+            "game_id" => 13,
+            "title" => "test",
+            "detail" => "test",
+            "created_at" => date("Y-m-d H:i:s"),
+            "updated_at" => date("Y-m-d H:i:s")
+        ]);
+        DB::table("posts")->insert([
+            "user_id" => 1,
+            "game_id" => 14,
+            "title" => "test",
+            "detail" => "test",
+            "created_at" => date("Y-m-d H:i:s"),
+            "updated_at" => date("Y-m-d H:i:s")
+        ]);
+        DB::table("posts")->insert([
+            "user_id" => 1,
+            "game_id" => 15,
+            "title" => "test",
+            "detail" => "test",
+            "created_at" => date("Y-m-d H:i:s"),
+            "updated_at" => date("Y-m-d H:i:s")
+        ]);
+        DB::table("posts")->insert([
+            "user_id" => 1,
+            "game_id" => 1625,
+            "title" => "test",
+            "detail" => "test",
+            "created_at" => date("Y-m-d H:i:s"),
+            "updated_at" => date("Y-m-d H:i:s")
+        ]);
     }
 }
