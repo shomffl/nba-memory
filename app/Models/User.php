c06a6a4087a23b9383ea0095776883a89e2f8767
sho watanabe <showatanabe0226@gmail.com>
2022-10-22 21:09:33 +0900

    [add]お気に入りチーム情報を管理するテーブルを作成

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function favoriteTeams()
+    {
+        return $this->belongsToMany(Team::class, "favorites", "user_id", "team_id");
+    }
 }
