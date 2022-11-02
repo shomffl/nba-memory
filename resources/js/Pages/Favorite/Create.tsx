b4674e375dca0cd4478820b488a5359e49d7ff57
sho watanabe <showatanabe0226@gmail.com>
2022-11-03 03:15:20 +0900

    [add]お気に入りチーム登録の通知にmantineのnortificationを使用

--- resources/js/Pages/Favorite/Create.tsx
+++ resources/js/Pages/Favorite/Create.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Authenticated from "@/Layouts/Authenticated";
 import { Head, Link, useForm } from "@inertiajs/inertia-react";
 import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+import { NotificationsProvider } from "@mantine/notifications";
+import { Check, X } from "tabler-icons-react";
+import { showNotification, cleanNotifications } from "@mantine/notifications";
 
 const Create = (props: any) => {
     const { teams, favoriteTeamIds } = props;
@@ -31,116 +34,164 @@ const Create = (props: any) => {
     };
 
     const addTeamId = (id: any) => {
-        console.log();
-        if (data.selectedTeamIds.length < 5) {
+        if (data.selectedTeamIds.length < 4) {
             setData("selectedTeamIds", [...data.selectedTeamIds, id]);
+            cleanNotifications();
+            showNotification({
+                title: "お気に入りチームが追加されました",
+                message:
+                    "他に選択したいチームがない場合は「Save」を押してください",
+                icon: <Check />,
+                color: "green",
+            });
+        } else if (data.selectedTeamIds.length == 4) {
+            cleanNotifications();
+            showNotification({
+                title: "お気に入り登録可能チームが上限に達しました",
+                message:
+                    "選択したチームに間違いない場合は、「Save」を押してください",
+                icon: <Check />,
+                color: "green",
+            });
         } else {
-            alert("お気に入りチームは5チームまでしか設定できません");
+            cleanNotifications();
+            showNotification({
+                title: "お気に入りチームは5チームまでしか選択できません",
+                message:
+                    "選択しているチームを外すか、「Save」を押して決定してください",
+                icon: <X />,
+                color: "red",
+            });
         }
     };
 
     const deleteTeamId = (id: any) => {
+        cleanNotifications();
+        showNotification({
+            title: "お気に入りチームが削除されました",
+            message:
+                "お気に入りチーム画面を使用するには、1チーム以上お気に入り登録する必要があります",
+            icon: <X />,
+            color: "red",
+        });
         const modefindDate = data.selectedTeamIds.filter((e: any) => e != id);
-        console.log(modefindDate);
         setData("selectedTeamIds", modefindDate);
     };
 
+    useEffect(() => {
+        if (data.selectedTeamIds.length != 0) {
+            showNotification({
+                title: "お気に入りチームが選択されています",
+                message:
+                    "他に選択したいチームがない場合は「Save」を押してください",
+                icon: <Check />,
+                color: "green",
+            });
+        }
+    }, []);
+
     return (
-        <Authenticated auth={props.auth} session={props.session} header={null}>
-            <Head title="PULL ORIGIN"></Head>
-            <div className="flex flex-col gap-10 mt-10 px-10">
-                <div className="flex items-center justify-between">
-                    {props.session.calendar == 1 ? (
-                        <Link
-                            href={route("games.index")}
-                            as="button"
-                            className="flex items-end border-b-2 border-red-900 pr-4 hover:scale-105 active:scale-100 duration-200"
-                        >
-                            <KeyboardDoubleArrowLeftIcon />
-                            <h2>Calendar</h2>
-                        </Link>
-                    ) : (
-                        <Link
-                            href={route("favorites.index")}
-                            as="button"
-                            className="flex items-end border-b-2 border-red-900 pr-4 hover:scale-105 active:scale-100 duration-200"
-                        >
-                            <KeyboardDoubleArrowLeftIcon />
-                            <h2>Calendar</h2>
-                        </Link>
-                    )}
-
-                    <h1 className="border-b-2 text-4xl w-max ">
-                        Favroite Team
-                    </h1>
-
-                    {data.selectedTeamIds.length != 0 ? (
-                        <button
-                            onClick={(e) => post(route("favorites.store"))}
-                            disabled={data.selectedTeamIds.length == 0}
-                            className="flex items-end border-b-2 border-gray-1000 pl-4 hover:scale-105 active:scale-100 duration-200 animate-bounce hover:animate-none"
-                        >
-                            <h2>Save</h2>
-                            <KeyboardDoubleArrowRightIcon />
-                        </button>
-                    ) : (
-                        <div className="flex items-end border-b-2 border-gray-1000 text-gray-400 border-opacity-50 pl-4">
-                            <h2>Save</h2>
-                            <KeyboardDoubleArrowRightIcon />
-                        </div>
-                    )}
-                </div>
-
-                <div className="grid grid-cols-5 gap-8 border-2 border-gray-300 py-7 rounded-2xl drop-shadow ">
-                    {data.selectedTeamIds.map((id: any) => (
-                        <div key={id}>
-                            <button
-                                onClick={(e) => deleteTeamId(id)}
-                                className="flex gap-5 justify-center items-center border-2 w-2/3 bg-gray-100 border-b-4 border-gray-300 mx-auto py-1 active:border-opacity-10 active:scale-95 duration-100"
+        <NotificationsProvider autoClose={5000}>
+            <Authenticated
+                auth={props.auth}
+                session={props.session}
+                header={null}
+            >
+                <Head title="PULL ORIGIN"></Head>
+                <div className="flex flex-col gap-10 mt-10 px-10">
+                    <div className="flex items-center justify-between">
+                        {props.session.calendar == 1 ? (
+                            <Link
+                                href={route("games.index")}
+                                as="button"
+                                className="flex items-end border-b-2 border-red-900 pr-4 hover:scale-105 active:scale-100 duration-200"
                             >
-                                <h1>{teams[id - 1].name}</h1>
-                                <img
-                                    className="max-h-8 w-7"
-                                    src={teams[id - 1].logo}
-                                />
-                            </button>
-                        </div>
-                    ))}
-                    {blankSpot()}
-                </div>
+                                <KeyboardDoubleArrowLeftIcon />
+                                <h2>Calendar</h2>
+                            </Link>
+                        ) : (
+                            <Link
+                                href={route("favorites.index")}
+                                as="button"
+                                className="flex items-end border-b-2 border-red-900 pr-4 hover:scale-105 active:scale-100 duration-200"
+                            >
+                                <KeyboardDoubleArrowLeftIcon />
+                                <h2>Calendar</h2>
+                            </Link>
+                        )}
 
-                <div className="grid grid-cols-5 gap-5 my-5">
-                    {teams.map((team: any) => (
-                        <div key={team.id}>
-                            {isIncludedIdInFavorites(team.id) ? (
+                        <h1 className="border-b-2 text-4xl w-max ">
+                            Favroite Team
+                        </h1>
+
+                        {data.selectedTeamIds.length != 0 ? (
+                            <button
+                                onClick={(e) => post(route("favorites.store"))}
+                                disabled={data.selectedTeamIds.length == 0}
+                                className="flex items-end border-b-2 border-gray-1000 pl-4 hover:scale-105 active:scale-100 duration-200"
+                            >
+                                <h2>Save</h2>
+                                <KeyboardDoubleArrowRightIcon />
+                            </button>
+                        ) : (
+                            <div className="flex items-end border-b-2 border-gray-1000 text-gray-400 border-opacity-50 pl-4">
+                                <h2>Save</h2>
+                                <KeyboardDoubleArrowRightIcon />
+                            </div>
+                        )}
+                    </div>
+
+                    <div className="grid grid-cols-5 gap-8 border-2 border-gray-300 py-7 rounded-2xl drop-shadow ">
+                        {data.selectedTeamIds.map((id: any) => (
+                            <div key={id}>
                                 <button
-                                    onClick={(e) => deleteTeamId(team.id)}
-                                    className="flex gap-5 justify-center items-center border w-2/3 bg-gray-500 opacity-60 mx-auto py-1 scale-95 duration-100"
-                                >
-                                    <h1>{team.name}</h1>
-                                    <img
-                                        className="max-h-7 w-6"
-                                        src={team.logo}
-                                    />
-                                </button>
-                            ) : (
-                                <button
-                                    onClick={(e) => addTeamId(team.id)}
+                                    onClick={(e) => deleteTeamId(id)}
                                     className="flex gap-5 justify-center items-center border-2 w-2/3 bg-gray-100 border-b-4 border-gray-300 mx-auto py-1 active:border-opacity-10 active:scale-95 duration-100"
-                                    key={team.id}
                                 >
-                                    <h1>{team.name}</h1>
+                                    <h1>{teams[id - 1].name}</h1>
                                     <img
                                         className="max-h-8 w-7"
-                                        src={team.logo}
+                                        src={teams[id - 1].logo}
                                     />
                                 </button>
-                            )}
-                        </div>
-                    ))}
+                            </div>
+                        ))}
+                        {blankSpot()}
+                    </div>
+
+                    <div className="grid grid-cols-5 gap-5 my-5">
+                        {teams.map((team: any) => (
+                            <div key={team.id}>
+                                {isIncludedIdInFavorites(team.id) ? (
+                                    <button
+                                        onClick={(e) => deleteTeamId(team.id)}
+                                        className="flex gap-5 justify-center items-center border w-2/3 bg-gray-500 opacity-60 mx-auto py-1 scale-95 duration-100"
+                                    >
+                                        <h1>{team.name}</h1>
+                                        <img
+                                            className="max-h-7 w-6"
+                                            src={team.logo}
+                                        />
+                                    </button>
+                                ) : (
+                                    <button
+                                        onClick={(e) => addTeamId(team.id)}
+                                        className="flex gap-5 justify-center items-center border-2 w-2/3 bg-gray-100 border-b-4 border-gray-300 mx-auto py-1 active:border-opacity-10 active:scale-95 duration-100"
+                                        key={team.id}
+                                    >
+                                        <h1>{team.name}</h1>
+                                        <img
+                                            className="max-h-8 w-7"
+                                            src={team.logo}
+                                        />
+                                    </button>
+                                )}
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            </div>
-        </Authenticated>
+            </Authenticated>
+        </NotificationsProvider>
     );
 };
 
