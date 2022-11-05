73f413c63212562a5df92d33c23127c78064ed4e
sho watanabe <showatanabe0226@gmail.com>
2022-11-05 22:26:58 +0900

    [update]ヘッダー下線部の色を白色に変更

--- resources/js/Components/NavLink.tsx
+++ resources/js/Components/NavLink.tsx
@@ -13,7 +13,7 @@ export default function NavLink({ href, active, children }: Props) {
             href={href}
             className={
                 active
-                    ? "text-white border-white inline-flex items-center px-1 pt-1 border-b-2 border-gray-1000 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-white transition duration-150 ease-in-out"
+                    ? "text-white border-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-white transition duration-150 ease-in-out"
                     : "text-gray-100 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-300 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
             }
         >
