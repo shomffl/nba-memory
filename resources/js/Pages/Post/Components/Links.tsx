13d11dde1c071c232b9232b174e03766bcc6ce04
sho watanabe <showatanabe0226@gmail.com>
2022-11-05 22:30:01 +0900

    [update]リンク作成時にtitleとurlを追加していないのプラスボタンを押した場合にnotificationを表示するように修正

--- resources/js/Pages/Post/Components/Links.tsx
+++ resources/js/Pages/Post/Components/Links.tsx
@@ -6,9 +6,10 @@ import {
     setDataByKeyValuePair,
 } from "@inertiajs/inertia-react";
 import { ActionIcon } from "@mantine/core";
-
 import { Trash } from "tabler-icons-react";
 import { Plus } from "tabler-icons-react";
+import { NotificationsProvider } from "@mantine/notifications";
+import { CustomNotification } from "../../../Components/CustomNotification";
 
 const Links = (props: {
     data: SendPost;
@@ -19,15 +20,28 @@ const Links = (props: {
     setLink: React.Dispatch<React.SetStateAction<Link>>;
 }) => {
     const { data, setData, link, setLink } = props;
+
     const addLink = () => {
-        const links = [...data.links, link];
-        console.log(links);
-        setData("links", links);
-        setLink({
-            id: 0,
-            title: "",
-            url: "",
-        });
+        if ((link.title && link.url) == "") {
+            CustomNotification({
+                title: "titleとurlを入力してください",
+                body: "Linkを追加するにはtitleとurlを入力する必要があります",
+                type: 0,
+            });
+        } else {
+            const links = [...data.links, link];
+            setData("links", links);
+            setLink({
+                id: 0,
+                title: "",
+                url: "",
+            });
+            CustomNotification({
+                title: "Linkが追加されました",
+                body: "削除したい場合はゴミ箱ボタンを押してください",
+                type: 1,
+            });
+        }
     };
 
     /**
@@ -40,75 +54,76 @@ const Links = (props: {
     };
 
     return (
-        <div>
-            <h2 className="mb-2">Link</h2>
-            <div className="flex">
-                <p className="w-4/12">title</p>
-                <p className="w-8/12">url</p>
-            </div>
+        <NotificationsProvider autoClose={5000}>
+            <div>
+                <h2 className="mb-2">Link</h2>
+                <div className="flex">
+                    <p className="w-4/12">title</p>
+                    <p className="w-8/12">url</p>
+                </div>
 
-            {data.links.map((link: any) => (
-                <div
-                    key={link.id}
-                    className="flex items-center gap-2 pt-2 pb-4"
-                >
-                    <div className="w-4/12 bg-white rounded py-2 px-3 shadow">
-                        {link.title}
-                    </div>
-                    <div className="w-8/12 bg-white rounded py-2 px-3 shadow">
-                        {link.url}
-                    </div>
-                    <ActionIcon
-                        className="hover:bg-gray-100 hover:scale-125 duration-200 active:scale-110"
-                        onClick={(e: any) => removeLink(link.id)}
+                {data.links.map((link: any) => (
+                    <div
+                        key={link.id}
+                        className="flex items-center gap-2 pt-2 pb-4"
                     >
-                        <Trash strokeWidth={1.5} color={"black"} />
+                        <div className="w-4/12 bg-white rounded py-2 px-3 shadow">
+                            {link.title}
+                        </div>
+                        <div className="w-8/12 bg-white rounded py-2 px-3 shadow">
+                            {link.url}
+                        </div>
+                        <ActionIcon
+                            className="hover:bg-gray-100 hover:scale-125 duration-200 active:scale-110"
+                            onClick={(e: any) => removeLink(link.id)}
+                        >
+                            <Trash strokeWidth={1.5} color={"black"} />
+                        </ActionIcon>
+                    </div>
+                ))}
+                <div className="flex items-center gap-2">
+                    <div className="w-4/12">
+                        <input
+                            type="text"
+                            value={link.title}
+                            className="w-full rounded border-gray-1000 focus:border-gray-1000 focus:ring-1 focus:ring-gray-1000 text-lg placeholder-black placeholder-opacity-30"
+                            placeholder="mavs highlight"
+                            onChange={(e) =>
+                                setLink({
+                                    id: data.links.length + 1,
+                                    title: e.target.value,
+                                    url: link.url,
+                                })
+                            }
+                        />
+                    </div>
+                    <div className="w-8/12">
+                        <input
+                            className="w-full rounded border-gray-1000 focus:border-gray-1000 focus:ring-1 focus:ring-gray-1000 text-lg placeholder-black placeholder-opacity-30"
+                            type="text"
+                            value={link.url}
+                            placeholder="https://youtube.com"
+                            onChange={(e) =>
+                                setLink({
+                                    id: data.links.length + 1,
+                                    title: link.title,
+                                    url: e.target.value,
+                                })
+                            }
+                        />
+                    </div>
+
+                    <ActionIcon
+                        className="hover:bg-gray-100 hover:scale-125 duration-200 active:scale-100"
+                        onClick={(e: any) => {
+                            addLink();
+                        }}
+                    >
+                        <Plus strokeWidth={1.5} color={"black"} />
                     </ActionIcon>
                 </div>
-            ))}
-            <div className="flex items-center gap-2">
-                <div className="w-4/12">
-                    <input
-                        type="text"
-                        value={link.title}
-                        className="w-full rounded border-gray-1000 focus:border-gray-1000 focus:ring-1 focus:ring-gray-1000 text-lg placeholder-black placeholder-opacity-30"
-                        placeholder="mavs highlight"
-                        onChange={(e) =>
-                            setLink({
-                                id: data.links.length + 1,
-                                title: e.target.value,
-                                url: link.url,
-                            })
-                        }
-                    />
-                </div>
-                <div className="w-8/12">
-                    <input
-                        className="w-full rounded border-gray-1000 focus:border-gray-1000 focus:ring-1 focus:ring-gray-1000 text-lg placeholder-black placeholder-opacity-30"
-                        type="text"
-                        value={link.url}
-                        placeholder="https://youtube.com"
-                        onChange={(e) =>
-                            setLink({
-                                id: data.links.length + 1,
-                                title: link.title,
-                                url: e.target.value,
-                            })
-                        }
-                    />
-                </div>
-
-                <ActionIcon
-                    className="hover:bg-gray-100 hover:scale-125 duration-200 active:scale-100"
-                    onClick={(e: any) => {
-                        addLink();
-                    }}
-                    disabled={(link.title && link.url) == ""}
-                >
-                    <Plus strokeWidth={1.5} color={"black"} />
-                </ActionIcon>
             </div>
-        </div>
+        </NotificationsProvider>
     );
 };
 
