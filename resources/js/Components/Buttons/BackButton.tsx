4c53f2db9544d514b9445ef839bbffc0f0348bfe
sho watanabe <showatanabe0226@gmail.com>
2022-11-15 02:18:30 +0900

    [update]戻るボタンのスタイルを画面サイズによって変更

--- resources/js/Components/Buttons/BackButton.tsx
+++ resources/js/Components/Buttons/BackButton.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Link } from "@inertiajs/inertia-react";
 import { Inertia } from "@inertiajs/inertia";
+import BreakPoints from "@/Pages/BreakPoints";
 
 const BackButton = (props: any) => {
     const { link } = props;
+
     return (
         <Link
             as="button"
             href={link}
-            className="bg-gray-1000 text-white hover:text-white hover:bg-red-900 shadow hover:shadow-2xl hover:scale-105 active:scale-100 px-5 py-1 rounded duration-200"
+            className={
+                BreakPoints()
+                    ? "bg-gray-1000 text-white hover:text-white hover:bg-red-900 shadow hover:shadow-2xl hover:scale-105 active:scale-100 px-5 py-1 rounded duration-200"
+                    : "bg-gray-1000 text-white active:bg-red-900 shadow active:scale-105 px-5 py-1 rounded"
+            }
         >
             Back
         </Link>
